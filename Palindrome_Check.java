bf3885cd6237999350bc824db5f61c127d94ab39
Amrish-16 <amrishnithiesh2131@gmail.com>
2025-12-02 21:14:19 +0530

    Add files via upload

---
+++ Palindrome_Check.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String rev = new StringBuilder(s).reverse().toString();
+        if (s.equals(rev))
+            System.out.println("yes");
+        else
+            System.out.println("no");
+        sc.close();
+    }
+}
