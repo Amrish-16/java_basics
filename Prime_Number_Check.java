49e62031808e1290df45bdaf102c43ccc38e7810
Amrish-16 <amrishnithiesh2131@gmail.com>
2025-12-02 21:08:13 +0530

    Add files via upload

---
+++ Prime_Number_Check.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N <= 1) {
+            System.out.println("no");
+            return;
+        }
+        boolean isPrime = true;
+        for (int i = 2; i * i <= N; i++) {
+            if (N % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(isPrime ? "yes" : "no");
+    }
+}
+
